149760062502485d8d721f900778f4dd3c6beef9
Juan Duque <juuanduuke@gmail.com>
2018-04-12 14:15:56 -0500

    upload script dowload certificate

--- js/template.js
+++ js/template.js
@@ -1,10 +1,27 @@
 $(".searchCertificate").on('click', function(event) {
     event.preventDefault();
-    axios.get("http://200.13.254.146/helpers/usuarios/certificado/1039421996", {})
+    axios.get("http://200.13.254.146/helpers/usuarios/certificado/"+$("input[name=documento]").val(), {})
         .then(function(response) {
             console.info(response.data);
         })
         .catch(function(error) {
             console.error(error);
+        });
+
+});
+
+$(".openCerticate").on('click', function(event) {
+    event.preventDefault();
+    var _this = $(this);
+    axios({
+        method: "get",
+        url : "http://200.13.254.146/generadorCertificados/index.php",
+        responseType:'arraybuffer'})
+        .then( function(response){
+            let blob = new Blob([response.data], { type:   'application/pdf' } );
+            let link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = 'Certificado.pdf';
+            link.click();
         });
 })
